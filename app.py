07467063e869e45e50451ed594405ff9ff9fdc87
Alessio Venturi <ale.venturi95@yahoo.it>
2023-08-08 22:57:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-def main():
-    print("Hello, DevOps!")
+from flask import Flask, jsonify
 
-if __name__ == "__main__":
-    main()
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify(message='Hello, World!')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
